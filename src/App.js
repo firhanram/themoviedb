847ae875a0586fd06abb7d021c7dea464f4f4adc
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-15 01:29:06 +0700

    setup slick-carousel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router} from 'react-router-dom'
 import { useEffect } from 'react';
 import { connect } from 'react-redux'
 import { fetchPopularMovies } from './actions/moviesAction'
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
 
 function App(props) {
   useEffect(() => {
