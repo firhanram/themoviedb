dd89f479e4bc0d995c622669b54431d2b8d09d9d
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-14 10:20:21 +0700

    add some files and remove unused files

---
+++ src/globalStyle.js
@@ -0,0 +1,16 @@
+import {createGlobalStyle} from 'styled-components'
+
+const globalStyle = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-size: 100%;
+        font-family: 'Source Sans Pro', sans-serif;
+    }
+`
+
+export default globalStyle
