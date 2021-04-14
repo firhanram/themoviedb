bf321f2bdff402ead3b46c3aa44b1a03db82dbb8
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-15 04:39:15 +0700

    add history library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import GlobalStyle from './globalStyle'
 import { Hero, Navbar, Popular } from './components'
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
