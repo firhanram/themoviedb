33107fde0751b97dcbeffe5e7b4e043a89c88ce3
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-15 00:12:52 +0700

    implementing redux

---
+++ src/actions/moviesAction.js
@@ -0,0 +1,7 @@
+import {
+    FETCH_POPULAR_MOVIES
+} from './types'
+
+export const fetchPopularMovies = () => async (dispatch) => {
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
+import reducers from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const enhancer = composeEnhancers(applyMiddleware(reduxThunk))
+const store = createStore(reducers, enhancer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
