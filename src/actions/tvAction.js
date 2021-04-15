f01c243242608048afa4e92c1755ac41a1e7a641
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-16 00:47:53 +0700

    Add tv shows redux actions

--- src/actions/tvAction.js
+++ src/actions/tvAction.js
@@ -1,4 +1,4 @@
-import { FETCH_POPULAR_TV_SHOWS } from './types'
+import { FETCH_AIRING_TODAY_TV_SHOWS, FETCH_ON_THE_AIR_TV_SHOWS, FETCH_POPULAR_TV_SHOWS, FETCH_TOP_RATED_TV_SHOWS } from './types'
 import themoviedb, { API_KEY } from '../apis/themoviedb'
 
 export const fetchPopularTvShows = () => async (dispatch) => {
@@ -6,6 +6,33 @@ export const fetchPopularTvShows = () => async (dispatch) => {
 
     dispatch({
         type: FETCH_POPULAR_TV_SHOWS,
+        payload: response.data
+    })
+}
+
+export const fetchAiringTodayTvShows = () => async (dispatch) => {
+    const response = await themoviedb.get(`/tv/airing_today?api_key=${API_KEY}&language=en-US&page=1`)
+
+    dispatch({
+        type: FETCH_AIRING_TODAY_TV_SHOWS,
+        payload: response.data
+    })
+}
+
+export const fetchOnTheAirTvShows = () => async (dispatch) => {
+    const response = await themoviedb.get(`/tv/on_the_air?api_key=${API_KEY}&language=en-US&page=1`)
+
+    dispatch({
+        type: FETCH_ON_THE_AIR_TV_SHOWS,
+        payload: response.data
+    })
+}
+
+export const fetchTopRatedTvShows = () => async (dispatch) => {
+    const response = await themoviedb.get(`/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
+
+    dispatch({
+        type: FETCH_TOP_RATED_TV_SHOWS,
         payload: response.data
     })
 }
