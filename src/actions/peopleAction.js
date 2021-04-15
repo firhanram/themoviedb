465f3b9c2adc580a9f602991d87123b032423ac2
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-16 04:35:48 +0700

    fix typo

--- src/actions/peopleAction.js
+++ src/actions/peopleAction.js
@@ -1,11 +1,11 @@
 import themoviedb, { API_KEY } from '../apis/themoviedb'
-import { FETCH_POPULAR_PEOPLE, FETCH_UPCOMING_MOVIES } from './types'
+import { FETCH_POPULAR_PEOPLE } from './types'
 
 export const fetchPopularPeople = () => async (dispatch) => {
     const response = themoviedb.get(`/person/popular?api_key=${API_KEY}&language=en-US&page=1`)
 
     dispatch({
-        type: FETCH_UPCOMING_MOVIES,
+        type: FETCH_POPULAR_PEOPLE,
         payload: response.data
     })
 }
