a0a7563bf142dc009dd388fb1bdc058ac813c7fa
Firhan Ramadhan <59257112+firhanram@users.noreply.github.com>
2021-04-16 00:51:03 +0700

    add reducer for handling tv shows data

--- src/reducers/tvReducer.js
+++ src/reducers/tvReducer.js
@@ -1,15 +1,40 @@
-import { FETCH_POPULAR_TV_SHOWS } from "../actions/types"
+import { 
+    FETCH_POPULAR_TV_SHOWS,
+    FETCH_AIRING_TODAY_TV_SHOWS,
+    FETCH_ON_THE_AIR_TV_SHOWS,
+    FETCH_TOP_RATED_MOVIES, 
+    FETCH_TOP_RATED_TV_SHOWS
+} from "../actions/types"
 
 const INITIAL_STATE = {
-    popular: []
+    popular: [],
+    airingToday: [],
+    onTheAir: [],
+    topRated: []
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case FETCH_POPULAR_TV_SHOWS:
             return {
                 ...state,
                 popular: action.payload
+            }
+        case FETCH_AIRING_TODAY_TV_SHOWS:
+            return {
+                ...state,
+                airingToday: action.payload
+            }
+        case FETCH_ON_THE_AIR_TV_SHOWS:
+            return {
+                ...state,
+                onTheAir: action.payload
+            }
+        case FETCH_TOP_RATED_TV_SHOWS:
+            return {
+                ...state,
+                topRated: action.payload
             }
         default:
             return state;
